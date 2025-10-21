99f874c8701877cd92d9b0b0f7e8b127a971e785
Heyru Jemal <heyru638@gmail.com>
2025-10-22 00:23:20 +0300

    auth

---
+++ inventory-management/app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from "@stackframe/stack";
+import Link from "next/link";
+
+export default function SignInPage() {
+  return <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 to-purple-100">
+    <div className="max-w-md w-full space-y-8" >
+        <SignIn/>
+            <Link href="/" className="text-blue-600 hover:bg-blue-600 hover:text-white mx-6 mt-18"> Go back to home</Link>
+
+    </div>
+    </div>;
+}
---
+++ inventory-management/prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import { use } from "react";
+const prisma = new PrismaClient();
+async function main() {
+  // Seed Categories
+  const demoUserId = "e531ab39-0f71-49f1-9d66-0db0b06aac0b";
+  // create sample products
+  await prisma.product.createMany({
+    data: Array.from({ length: 25 }).map((_, i) => ({
+      userId: demoUserId,
+      name: `Product ${i + 1}`,
+      price: (Math.random() * 90 + 10).toFixed(2),
+      quantity: Math.floor(Math.random() * 20) + 1,
+      lowStackAt: 5,
+      createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * (i * 5)),
+    })),
+  });
+  console.log("Seeding completed successfully.");
+  console.log(`25 sample products created for user ID: ${demoUserId}`);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
