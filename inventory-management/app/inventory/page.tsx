b7a43e592d9df352da8f4485e3b454feac90f9be
Heyru Jemal <heyru638@gmail.com>
2025-10-25 00:21:35 +0300

    inventory page

---
+++ inventory-management/app/inventory/page.tsx
@@ -0,0 +1,114 @@
+import SideBar from "@/components/sideBar";
+import { deleteProduct } from "@/lib/actions/products";
+import getCurrentUser from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+
+export default async function InventoryPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const user = await getCurrentUser();
+  const userId = user.id;
+  const params = await searchParams;
+  const q = (params.q ?? "").trim();
+  const totalProducts = await prisma.product.findMany({
+    where: { userId, name: { contains: q, mode: "insensitive" } },
+  });
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <SideBar currentPath="/inventory" />
+      <main className="ml-64 p-8">
+        <div className="mb-8">
+          <div className="flex items-center justify-between ">
+            <div>
+              <h1 className="text-2xl font-semibold text-gray-900">
+                Inventory
+              </h1>
+              <p className="text-sm text-gray-500">
+                Manage your products and track inventory levels.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="space-y-6">
+          {/* search bar */}
+          <div className="bg-white rounded-lg border border-gray-200 p-6">
+            <form className="flex gap-2" action="/inventory" method="GET">
+              <input
+                name="q"
+                placeholder="Search products..."
+                className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focuse:border-transparent "
+              />
+              <button className="px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700">
+                Search
+              </button>
+            </form>
+          </div>
+          {/* products table */}
+          <div className="bg-white rounded-lg border border-gray-200 overflow-hidden">
+            <table className="w-full">
+              <thead className="">
+                <tr className="bg-gray-50">
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                    Name
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                    SKU
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                    Price($)
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                    Quantity
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                    Low Stock At
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {totalProducts.map((product, key) => (
+                  <tr key={key} className="hover:bg-gray-50">
+                    <td className="px-6 text-sm py-4 text-gray-500">
+                      {product.name}
+                    </td>
+                    <td className="px-6 text-sm py-4 text-gray-500">
+                      {product.sku || "-"}
+                    </td>
+                    <td className="px-6 text-sm py-4 text-gray-500">
+                      {product.price.toFixed(2)}
+                    </td>
+                    <td className="px-6 text-sm py-4 text-gray-500">
+                      {product.quantity}
+                    </td>
+                    <td className="px-6 text-sm py-4 text-gray-500">
+                      {product.LowStockAt || "-"}
+                    </td>
+                    <td className="px-6 text-sm py-4 text-gray-500">
+                      <form
+                        action={async (formData: FormData) => {
+                          "use server";
+                          await deleteProduct(formData);
+                        }}
+                      >
+                        <input type="hidden" name="id" value={product.id} />
+                        <button className="text-red-600 hover:text-white hover:bg-red-700 py-1 px-2 rounded-lg">
+                          Delete
+                        </button>
+                      </form>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
