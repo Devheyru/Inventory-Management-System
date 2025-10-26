d4aa8926d1b0811cce422581bc9da89fc3291a8f
Heyru Jemal Mohammed <162872330+Devheyru@users.noreply.github.com>
2025-10-27 00:18:09 +0300

    Update page.tsx

--- inventory-management/app/sign-in/page.tsx
+++ inventory-management/app/sign-in/page.tsx
@@ -5,7 +5,7 @@ export default function SignInPage() {
   return <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 to-purple-100">
     <div className="max-w-md w-full space-y-8" >
         <SignIn/>
-            <Link href="/" className="text-blue-600 hover:bg-blue-600 hover:text-white mx-6 mt-18"> Go back to home</Link>
+            <Link href="/dashboard" className="text-blue-600 hover:bg-blue-600 hover:text-white mx-6 mt-18"> Go back to home</Link>
 
     </div>
     </div>;
