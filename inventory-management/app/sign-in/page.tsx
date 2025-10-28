122713d93e07b5fc30139f6c931ac8dd23bad7ee
Heyru Jemal <heyru638@gmail.com>
2025-10-28 15:20:49 +0300

    m

--- inventory-management/app/sign-in/page.tsx
+++ inventory-management/app/sign-in/page.tsx
@@ -14,18 +14,12 @@ export default function SignInPage() {
     if (user) {
       router.push("/dashboard");
     }
-  }, [user, router]);
+  }, []);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 to-purple-100">
       <div className="max-w-md w-full space-y-8">
-        <SignIn automaticRedirect={true} />
-        <Link
-          href="/"
-          className="text-blue-600 hover:bg-blue-600 hover:text-white mx-6 mt-18 inline-block text-center"
-        >
-          Go back to home
-        </Link>
+        <SignIn />
       </div>
     </div>
   );
